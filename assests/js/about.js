59648b1081a1f4a11c49154c8df08503d86afa39
afsar1ansel <mdafsaransal@gmail.com>
2024-12-19 16:04:58 +0530

    mobile for about

--- assests/js/about.js
+++ assests/js/about.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
           items: 1.2, // Show 1.5 items on small tablets
         },
         768: {
-          items: 1.5, // Show 2 full items and part of the next on medium screens
+          items: 2, // Show 2 full items and part of the next on medium screens
         },
         1024: {
           items: 2.6, // Show 2 full items and half of the next on larger screens
