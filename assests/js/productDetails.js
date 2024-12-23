53b2c87d164858a9b02e0df84d785eb15bd729e6
afsar1ansel <mdafsaransal@gmail.com>
2024-12-23 22:05:17 +0530

    details

--- assests/js/productDetails.js
+++ assests/js/productDetails.js
@@ -1,3 +1,32 @@
+$(document).ready(function () {
+  $(".carouselSection-owl").owlCarousel({
+    // items: 2.5,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    //   autoplayHoverPause: true,
+    dots: false,
+    nav: true,
+    margin: 10,
+    navText: [`←`, `→`],
+    responsive: {
+      0: {
+        items: 1, // Show 1 full item and a bit of the next on small screens
+      },
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 2, // Show 2 full items and part of the next on medium screens
+      },
+      1024: {
+        items: 3.6, // Show 2 full items and half of the next on larger screens
+      },
+    },
+  });
+});
+
+
 function getUrlParameter(name) {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(name);
