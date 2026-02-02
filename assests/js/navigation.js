f02c8f4b4b480ff0611cc3d1eac78b1eef90c224
afsar1ansel <mdafsaransal@gmail.com>
2026-02-02 16:37:03 +0530

    image

--- assests/js/navigation.js
+++ assests/js/navigation.js
@@ -138,17 +138,9 @@ nav.innerHTML = `
 
   <div><a href="download.html" id="blog">DOWNLOAD</a></div>
   <div><a href="contact.html" id="contactUs">CONTACT US</a></div>
-  <div id="getQuote">GET A QUOTE 
-    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" viewBox="0 0 12 13" fill="none">
-    <path
-  d="M1.98771 1.50635C1.98771 1.24113 2.09307 0.986777 2.28061 0.799241C2.46814 0.611705 2.7225 0.506348 2.98771 0.506348H10.9877C11.2529 0.506348 11.5073 0.611705 11.6948 0.799241C11.8824 0.986777 11.9877 1.24113 11.9877 1.50635V9.50635C11.9877 9.77156 11.8824 10.0259 11.6948 10.2135C11.5073 10.401 11.2529 10.5063 10.9877 10.5063C10.7225 10.5063 10.4681 10.401 10.2806 10.2135C10.0931 10.0259 9.98771 9.77156 9.98771 9.50635V3.92035L1.69471 12.2133C1.50611 12.3955 1.25351 12.4963 0.991311 12.494C0.729114 12.4917 0.478302 12.3866 0.292894 12.2012C0.107485 12.0158 0.00231622 11.7649 3.78025e-05 11.5027C-0.00224062 11.2406 0.0985542 10.9879 0.280712 10.7993L8.57371 2.50635H2.98771C2.7225 2.50635 2.46814 2.40099 2.28061 2.21345C2.09307 2.02592 1.98771 1.77156 1.98771 1.50635Z"
-  fill="black"
-/>;
-    </svg>
-  </div>
+  
 </div>
 `;
-
 
 // Enquiry Modal Implementation
 const modalHTML = `
@@ -248,29 +240,41 @@ const modalStyles = `
 </style>
 `;
 
-document.body.insertAdjacentHTML('beforeend', modalHTML);
-document.head.insertAdjacentHTML('beforeend', modalStyles);
+document.body.insertAdjacentHTML("beforeend", modalHTML);
+document.head.insertAdjacentHTML("beforeend", modalStyles);
 
 // Modal Logic
-window.openEnquiryModal = function() {
-  document.getElementById('enquiryModal').style.display = 'flex';
-}
+window.openEnquiryModal = function () {
+  document.getElementById("enquiryModal").style.display = "flex";
+};
 
-document.querySelector('.close-modal').addEventListener('click', function() {
-  document.getElementById('enquiryModal').style.display = 'none';
+document.querySelector(".close-modal").addEventListener("click", function () {
+  document.getElementById("enquiryModal").style.display = "none";
 });
 
-window.addEventListener('click', function(event) {
-  if (event.target == document.getElementById('enquiryModal')) {
-    document.getElementById('enquiryModal').style.display = 'none';
+window.addEventListener("click", function (event) {
+  if (event.target == document.getElementById("enquiryModal")) {
+    document.getElementById("enquiryModal").style.display = "none";
   }
 });
 
 // Attach to Get Quote button
-const getQuoteBtn = document.getElementById('getQuote');
+const getQuoteBtn = document.getElementById("getQuote");
 if (getQuoteBtn) {
-    getQuoteBtn.addEventListener('click', function() {
-        openEnquiryModal();
-    });
-    getQuoteBtn.style.cursor = 'pointer';
+  getQuoteBtn.addEventListener("click", function () {
+    openEnquiryModal();
+  });
+  getQuoteBtn.style.cursor = "pointer";
 }
+
+
+
+
+// line 141 <div id="getQuote">GET A QUOTE 
+//     <svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" viewBox="0 0 12 13" fill="none">
+//     <path
+//   d="M1.98771 1.50635C1.98771 1.24113 2.09307 0.986777 2.28061 0.799241C2.46814 0.611705 2.7225 0.506348 2.98771 0.506348H10.9877C11.2529 0.506348 11.5073 0.611705 11.6948 0.799241C11.8824 0.986777 11.9877 1.24113 11.9877 1.50635V9.50635C11.9877 9.77156 11.8824 10.0259 11.6948 10.2135C11.5073 10.401 11.2529 10.5063 10.9877 10.5063C10.7225 10.5063 10.4681 10.401 10.2806 10.2135C10.0931 10.0259 9.98771 9.77156 9.98771 9.50635V3.92035L1.69471 12.2133C1.50611 12.3955 1.25351 12.4963 0.991311 12.494C0.729114 12.4917 0.478302 12.3866 0.292894 12.2012C0.107485 12.0158 0.00231622 11.7649 3.78025e-05 11.5027C-0.00224062 11.2406 0.0985542 10.9879 0.280712 10.7993L8.57371 2.50635H2.98771C2.7225 2.50635 2.46814 2.40099 2.28061 2.21345C2.09307 2.02592 1.98771 1.77156 1.98771 1.50635Z"
+//   fill="black"
+// />
+//     </svg>
+//   </div>
