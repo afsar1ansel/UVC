1905c4e5918814797bea7e515a61caa372c0a126
Md Afsar Ansel <mdafsaransel@Mds-MacBook-Pro.local>
2026-07-10 17:29:46 +0530

    index

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  var owl = $(".owl-carousel");
+  var itemCount = owl.find(".item").length;
+
+  owl.owlCarousel({
     items: 1, // Number of items to show at once
-    loop: true, // Infinite loop
+    loop: itemCount > 1, // Only loop if there is more than 1 item
     margin: 10, // Margin between items
     nav: false, // Disable next/prev buttons
-    dots: true, // Show pagination dots
-    autoplay: true, // Enable autoplay
+    dots: itemCount > 1, // Only show pagination dots if there is more than 1 item
+    autoplay: itemCount > 1, // Only autoplay if there is more than 1 item
     autoplayTimeout: 3000, // Time between auto slides
     smartSpeed: 3000, // Speed of the transition between slides
-    mouseDrag: false, // Disable dragging for vertical slides
-    touchDrag: false, // Disable touch dragging for vertical slides
+    mouseDrag: itemCount > 1, // Disable dragging for single slide
+    touchDrag: itemCount > 1, // Disable touch dragging for single slide
     animateIn: "fadeIn", // Optional: Fade transition in
     animateOut: "fadeOut", // Optional: Fade transition out
     responsiveClass: true,
